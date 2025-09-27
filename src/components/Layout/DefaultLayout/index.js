b68774b4486a5a92736019c88349fddb511e980f
DMiTri904 <169603869+DMiTri904@users.noreply.github.com>
2025-09-27 16:04:17 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
+import classNames from "classnames/bind"
 import Header from "~/components/Layout/components/Header"
 import Sideabar from "./Sidebar"
+import styles from './DefaultLayout.module.scss'
+
+const cx = classNames.bind(styles)
 
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <Sideabar />
-        <div className="content">
+        <div className={cx('content')}>
           {children}
         </div>
       </div>
